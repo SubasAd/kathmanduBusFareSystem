335abfc1fef677eb24adc10c3c94d8ebfcf76a78
subasAd <subasadhikari0025@gmail.com>
2023-12-26 12:51:04 +0545

    Added Logger

--- src/main/java/com/subasadhikari/kathmanduvalleybusfarecalculationsystem/globalExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/subasadhikari/kathmanduvalleybusfarecalculationsystem/globalExceptionHandler/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ package com.subasadhikari.kathmanduvalleybusfarecalculationsystem.globalExceptio
 import com.subasadhikari.kathmanduvalleybusfarecalculationsystem.busRoute.exceptions.NoRouteFoundException;
 import com.subasadhikari.kathmanduvalleybusfarecalculationsystem.busStop.exceptions.BusStopAlreadyExistsException;
 import com.subasadhikari.kathmanduvalleybusfarecalculationsystem.busStop.exceptions.NoBusStopFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,21 +13,25 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-
+        private static final
+        Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
         @ExceptionHandler({NoRouteFoundException.class})
         public ResponseEntity<Object> handleRouteNotFoundException(NoRouteFoundException exception) {
+            logger.error(exception.getMessage());
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(exception.getMessage());
         }
         @ExceptionHandler({NoBusStopFoundException.class})
         public ResponseEntity<Object> handleBusStopNotFoundException(NoBusStopFoundException exception) {
+            logger.error(exception.getMessage());
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(exception.getMessage());
         }
     @ExceptionHandler({BusStopAlreadyExistsException.class})
     public ResponseEntity<Object> handleBusStopALreadyExistsException(BusStopAlreadyExistsException exception) {
+        logger.error(exception.getMessage());
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(exception.getMessage());
@@ -33,6 +39,7 @@ public class GlobalExceptionHandler {
 
         @ExceptionHandler({RuntimeException.class})
         public ResponseEntity<Object> handleRuntimeException(RuntimeException exception) {
+            logger.error(exception.getMessage());
             return ResponseEntity
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(exception.getMessage());
